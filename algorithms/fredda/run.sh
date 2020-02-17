76d3a5972b019bb0e883778999a67d778fa63cd7
Tom Klaver <tomklav@gmail.com>
2020-02-17 14:32:05 +0100

    fix

--- algorithms/fredda/run.sh
+++ algorithms/fredda/run.sh
@@ -4,7 +4,7 @@ INPUT_PATH="/data/input/test_data"
 OUTPUT_PATH="/data/output"
 
 FREQ_REF_NAME='low' 
-FREQ_REF=$(python3 get_fil_header.py $INPUT_PATH $FREQ_REF_NAME)
+FREQ_REF=$(python3 /get_fil_header.py $INPUT_PATH $FREQ_REF_NAME)
 
 cudafdmt -t 512 -d 4096 -x 8 -o $TMP_PATH /data/input/test_data
 
