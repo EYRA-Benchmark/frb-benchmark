7cd87e7c8e82f6b766207c37f1f6bad7a83b076b
ARTS <arts@arts041.apertif>
2020-03-18 08:45:34 +0000

    changes basedir path for arts

--- algorithms/fredda/run.sh
+++ algorithms/fredda/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-TMP_PATH="/tmp/fredda"
-INPUT_PATH="/data/input/test_data"
-OUTPUT_PATH="/data/output"
+
+BASENAME=/tank/users/connor/eyra/data # this is /data in the master branch    
+TMP_PATH="$BASENAME/fredda"
+INPUT_PATH="$BASENAME/input/test_data"
+OUTPUT_PATH="$BASENAME/output"
 
 FREQ_REF_NAME='low' 
-FREQ_REF=$(python3 /get_fil_header.py $INPUT_PATH $FREQ_REF_NAME)
+FREQ_REF=$(python3 ./get_fil_header.py $INPUT_PATH $FREQ_REF_NAME)
 
-cudafdmt -t 512 -d 4096 -x 8 -o $TMP_PATH /data/input/test_data
+cudafdmt -t 512 -d 4096 -x 8 -o $TMP_PATH $BASENAME/input/test_data
 
-tail -n +2 $TMP_PATH | awk -F" " -v FREQ_REF="$FREQ_REF" '{ print $6 " " $1 " " $3 " " $4 " " FREQ_REF }' > /data/output
+tail -n +2 $TMP_PATH | awk -F" " -v FREQ_REF="$FREQ_REF" '{ print $6 " " $1 " " $3 " " $4 " " FREQ_REF }' > $OUTPUT_PATH
 
