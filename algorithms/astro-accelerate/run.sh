d5111c7cb04b6a1d6098709f4fc97f62216fa299
ARTS <arts@arts041.apertif>
2020-03-18 08:47:01 +0000

    changes basedir path for arts

--- algorithms/astro-accelerate/run.sh
+++ algorithms/astro-accelerate/run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 FREQ_REF_NAME='high' 
 FREQ_REF=$(python3 get_fil_header.py $file $FREQ_REF_NAME)
+BASENAME=/tank/users/connor/eyra/data/ # this is /data in the master branch
+OUTPUT_PATH=$BASENAME/output
 
-python3 /app/find_candidates.py /data/input/test_data
+python3 /app/find_candidates.py $BASENAME/input/test_data
 
-cat /tmp/output | awk -F" " -v FREQ_REF="$FREQ_REF" '{ print $0 " " FREQ_REF }' > /data/output
+cat /tmp/output | awk -F" " -v FREQ_REF="$FREQ_REF" '{ print $0 " " FREQ_REF }' > $OUTPUT_PATH
 
