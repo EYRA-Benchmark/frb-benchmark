69a07f2300866b819064e6e26fa6811b8aa18d76
ARTS <arts@arts041.apertif>
2020-04-07 14:20:13 +0000

    decreased fredda s/n thresh from 8 to 6

--- algorithms/fredda/run.sh
+++ algorithms/fredda/run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 BASENAME=/tank/users/connor/eyra/data # this is /data in the master branch    
-TMP_PATH="$BASENAME/fredda"
+#BASENAME=/data
+TMP_PATH="/opt/frbbench/fredda_tmp"
+TMP_PATH=$BASENAME/fredda_tmp
 INPUT_PATH="$BASENAME/input/test_data"
-OUTPUT_PATH="$BASENAME/output"
+OUTPUT_PATH="$BASENAME/fredda"
 
 FREQ_REF_NAME='low' 
 FREQ_REF=$(python3 ./get_fil_header.py $INPUT_PATH $FREQ_REF_NAME)
 
-cudafdmt -t 512 -d 4096 -x 8 -o $TMP_PATH $BASENAME/input/test_data
+cudafdmt -t 512 -d 4096 -x 6 -o $TMP_PATH $BASENAME/input/test_data
 
 tail -n +2 $TMP_PATH | awk -F" " -v FREQ_REF="$FREQ_REF" '{ print $6 " " $1 " " $3 " " $4 " " FREQ_REF }' > $OUTPUT_PATH
 
