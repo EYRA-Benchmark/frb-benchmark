27bdc90bd573a258305faaced1ae089e507b04ff
Leon Oostrum <leonoostrum@gmail.com>
2020-07-08 15:55:33 +0000

    Update FREDDA run script for max DM=10000

--- algorithms/fredda/run.sh
+++ algorithms/fredda/run.sh
@@ -10,7 +10,13 @@ OUTPUT_PATH="$BASENAME/fredda"
 FREQ_REF_NAME='low' 
 FREQ_REF=$(python3 ./get_fil_header.py $INPUT_PATH $FREQ_REF_NAME)
 
-cudafdmt -t 512 -d 4096 -x 6 -o $TMP_PATH $BASENAME/input/test_data
+#g: cuda device
+#t: samples per block
+#x: S/N threshold
+#d: number of dispersion trials in units of sampling time; ASKAP data has 1.2656 ms sampling, freq range 1165-1500
+# DM 10000 results in delay of 12.13 s = 9584 trials 
+# then round up to nearest multiple of samples per block: 9728
+cudafdmt -g 0 -t 512 -d 9728 -x 6 -o $TMP_PATH $BASENAME/input/test_data
 
 tail -n +2 $TMP_PATH | awk -F" " -v FREQ_REF="$FREQ_REF" '{ print $6 " " $1 " " $3 " " $4 " " FREQ_REF }' > $OUTPUT_PATH
 
