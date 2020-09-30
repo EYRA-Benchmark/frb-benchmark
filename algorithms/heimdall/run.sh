f17c05d79306b0ee87aa5dc181e04bdfa54360b1
ARTS <arts@arts041.apertif>
2020-09-30 13:36:20 +0000

    dm_to changed to dm_tol

--- algorithms/heimdall/run.sh
+++ algorithms/heimdall/run.sh
@@ -11,6 +11,6 @@ FREQ_REF_NAME='high'
 FREQ_REF=$(python get_fil_header.py $INPUT_PATH $FREQ_REF_NAME)
 
 mkdir -p $TMP_PATH
-heimdall -v -detect_thresh 6.0 -dm_to 1.01 -nsamps_gulp 1048576 -f $INPUT_PATH -dm 0. 10000. -rfi_no_narrow -rfi_no_broad -output_dir $TMP_PATH
+heimdall -v -detect_thresh 6.0 -dm_tol 1.01 -nsamps_gulp 1048576 -f $INPUT_PATH -dm 0. 10000. -rfi_no_narrow -rfi_no_broad -output_dir $TMP_PATH
 # cols: DM, S/N, TIME, LOG_2_DOWNSAMPLE, FREQ_REF
 cat $TMP_PATH/*.cand | awk -F" " -v FREQ_REF="$FREQ_REF" '{ print $6 " " $1 " " $3 " " 2^$4 " " FREQ_REF }' > $OUTPUT_PATH
